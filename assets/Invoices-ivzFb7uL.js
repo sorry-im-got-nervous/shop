83f5d8dd0ce4671bdfff54e27a62026a00b88277
sorry-im-got-nervous <117665318+sorry-im-got-nervous@users.noreply.github.com>
2024-07-14 15:18:34 +0800

    Delete assets directory

--- assets/Invoices-ivzFb7uL.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,e as n,o as r,c as a,a as c}from"./index-BbWf6Gvq.js";const _={setup(){n((e,o)=>{if(!window.confirm("Вы действительно хотите уйти?"))return!1})}},f=c("h1",null,"Покупки",-1),i=[f];function d(e,o,s,l,p,u){return r(),a("div",null,i)}const h=t(_,[["render",d]]);export{h as default};
