9022923a4fd7a1118ca22762c9184302ea759323
sorry-im-got-nervous <117665318+sorry-im-got-nervous@users.noreply.github.com>
2024-07-19 19:22:12 +0800

    Add files via upload

---
+++ assets/Invoices-Ch4CAFlK.js
@@ -0,0 +1,1 @@
+import{_ as t,e as n,o as r,c as a,a as c}from"./index-CSkqQKr3.js";const _={setup(){n((e,o)=>{if(!window.confirm("Вы действительно хотите уйти?"))return!1})}},f=c("h1",null,"Покупки",-1),i=[f];function d(e,o,s,l,p,u){return r(),a("div",null,i)}const h=t(_,[["render",d]]);export{h as default};
